320abfc2a601bb5ab76e59beea34e217d177ca31
brandonartz <brandon.artz@gmail.com>
2022-03-25 18:31:29 -0400

    Added dirt block

---
+++ src/main/java/net/blocks/OddBlocksTier1.java
@@ -0,0 +1,9 @@
+package net.blocks;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+
+public class OddBlocksTier1 {
+    public static final Block ODD_DIRT = new Block(FabricBlockSettings.of(Material.SOIL).strength(1.0f));
+}
--- src/main/java/net/fabricmc/oddblocks/OddBlocksMod.java
+++ src/main/java/net/fabricmc/oddblocks/OddBlocksMod.java
@@ -1,6 +1,13 @@
 package net.fabricmc.oddblocks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.blocks.OddBlocksTier1;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +17,14 @@ public class OddBlocksMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
-	@Override
+    @Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.BLOCK, new Identifier("oddblocks", "odd_dirt_block"), OddBlocksTier1.ODD_DIRT);
+		Registry.register(Registry.ITEM, new Identifier("oddblocks", "odd_dirt_block"), new BlockItem(OddBlocksTier1.ODD_DIRT, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
