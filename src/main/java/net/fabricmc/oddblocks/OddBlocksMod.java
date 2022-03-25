9cb54bc708ac60c583a5fcda48b312bc002994ee
brandonartz <brandon.artz@gmail.com>
2022-03-25 19:30:57 -0400

    Playing with custom blocks

--- src/main/java/net/fabricmc/oddblocks/OddBlocksMod.java
+++ src/main/java/net/fabricmc/oddblocks/OddBlocksMod.java
@@ -2,6 +2,8 @@ package net.fabricmc.oddblocks;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -17,6 +19,8 @@ public class OddBlocksMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	public static final OddBlocksTier1 ODD_DIRT = new OddBlocksTier1(FabricBlockSettings.of(Material.SOIL).strength(1.0f));
+	
     @Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -24,7 +28,7 @@ public class OddBlocksMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-		Registry.register(Registry.BLOCK, new Identifier("oddblocks", "odd_dirt_block"), OddBlocksTier1.ODD_DIRT);
-		Registry.register(Registry.ITEM, new Identifier("oddblocks", "odd_dirt_block"), new BlockItem(OddBlocksTier1.ODD_DIRT, new FabricItemSettings().group(ItemGroup.MISC)));
+		Registry.register(Registry.BLOCK, new Identifier("oddblocks", "odd_dirt_block"), ODD_DIRT);
+		Registry.register(Registry.ITEM, new Identifier("oddblocks", "odd_dirt_block"), new BlockItem(ODD_DIRT, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
