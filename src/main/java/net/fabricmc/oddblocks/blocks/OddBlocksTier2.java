2e2ade6dd73f69c37d69411876f556d7bad8a4b6
brandonartz <brandon.artz@gmail.com>
2022-04-01 17:51:03 -0400

    Removed LIMIT for block state due to errors

--- src/main/java/net/fabricmc/oddblocks/blocks/OddBlocksTier2.java
+++ src/main/java/net/fabricmc/oddblocks/blocks/OddBlocksTier2.java
@@ -12,31 +12,12 @@ import net.minecraft.world.WorldAccess;
 @Mixin(Block.class)
 public class OddBlocksTier2 extends Block {
 
-    //Set maximum mining count
-    public static final int maxMining = 199;
-    public static final IntProperty LIMIT = IntProperty.of("limit", 0, maxMining);
-    
-    public OddBlocksTier2(Settings settings, int max) {
-        super(settings);
-        setDefaultState(getStateManager().getDefaultState().with(LIMIT, max));
-    }
-
     public OddBlocksTier2(Settings settings) {
         super(settings);
-        setDefaultState(getStateManager().getDefaultState().with(LIMIT, maxMining));
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(LIMIT);
     }
 
     @Override
     public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
-        if (state.get(LIMIT) != -1 && state.get(LIMIT) > 0) {
-            world.setBlockState(pos, state.with(LIMIT, state.get(LIMIT) - 1), Block.NOTIFY_ALL);
-        } else if (state.get(LIMIT) == -1) {
-            world.setBlockState(pos, state, Block.NOTIFY_ALL);
-        }
+        world.setBlockState(pos, state, Block.NOTIFY_ALL);
     }
 }
