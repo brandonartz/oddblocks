a7cfcb26ed6d0e2cb2a73f49474fa09ecc4da057
brandonartz <brandon.artz@gmail.com>
2022-04-02 21:33:18 -0400

    Automine texture and model completed

--- src/main/java/net/fabricmc/oddblocks/blocks/OddAutoMiner.java
+++ src/main/java/net/fabricmc/oddblocks/blocks/OddAutoMiner.java
@@ -7,19 +7,18 @@ import javax.annotation.Nullable;
 import net.fabricmc.oddblocks.OddBlocksMod;
 import net.fabricmc.oddblocks.block_entity.OddAutoMinerBlockEntity;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.block.AbstractFurnaceBlock;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class OddAutoMiner extends BlockWithEntity {
@@ -73,6 +72,11 @@ public class OddAutoMiner extends BlockWithEntity {
         }
 
         world.createAndScheduleBlockTick(pos, this, 40);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 0.9f, 1f, 0.9f);
     }
 
     // @Override
